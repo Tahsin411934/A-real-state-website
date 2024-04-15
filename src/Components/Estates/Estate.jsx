8b9524c1b5f82dec07a72e350896ba4af0f997ad
Tahsin411934 <tahsin.mail.0503@gmail.com>
2024-04-15 21:02:31 +0600

    estate section addd

--- src/Components/Estates/Estate.jsx
+++ src/Components/Estates/Estate.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Estate = ({ estate }) => {
   const {
     estate_title,
@@ -13,23 +15,24 @@ const Estate = ({ estate }) => {
   console.log(estate);
   return (
     <div className="container mx-auto font-display">
-      <div className="card w-96 bg-base-100 shadow-xl">
+      <div className="card w-96 bg-base-100 shadow-xl bg-contain bg-center">
         <figure className=" pt-10">
           <img
             src={image}
             alt="House Image"
-            className="rounded-xl"
+            className="rounded-xl h-[300px] "
           />
         </figure>
         <div className="card-body items-center text-center">
           <h2 className="card-title">{estate_title} |  {segment_name}</h2>
-          <div className="flex items-center gap-8">
+          <div className="flex items-center gap-10">
           <p>{location}</p>
-          <p>{price}</p>
+          <p>Price: {price}</p>
           </div>
           
+          
           <div className="card-actions">
-            <button className="btn btn-primary">Buy Now</button>
+           <Link><button className="btn btn-primary">View Property</button></Link> 
           </div>
         </div>
       </div>
