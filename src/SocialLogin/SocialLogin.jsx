63b78b43d3e706a42b8e24b6f8493f0d085e5c1f
Tahsin411934 <tahsin.mail.0503@gmail.com>
2024-04-17 22:01:15 +0600

    final

--- src/SocialLogin/SocialLogin.jsx
+++ src/SocialLogin/SocialLogin.jsx
@@ -19,13 +19,23 @@ const SocialLogin = () => {
         toast.error(error.Message);
     })
 }
+   const handleGithubLogin=()=>{
+    setLoading(true)
+    githubLogin()
+    .then(()=>{
+        navigate("/")
+    })
+    .catch((error)=>{
+        toast.error(error.Message);
+    })
+}
     
     return (
         <div>
             <div className="w-[70%] mx-auto"><p>---------- Login with  ---------</p></div>
             <div className="form-control mt-6">
           <button onClick={handleGoogleLogin} type="submit" className="btn text-xl bg-slate-300"><FcGoogle />Google </button>
-          <button onClick={()=>githubLogin()} type="submit" className="btn text-xl mt-5 bg-slate-300"><FaGithub />Github </button>
+          <button onClick={handleGithubLogin} type="submit" className="btn text-xl mt-5 bg-slate-300"><FaGithub />Github </button>
         </div>
         <ToastContainer></ToastContainer>
         </div>
